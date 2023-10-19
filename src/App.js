fd6368e69b25d15a91da8bafb943dd2b9fb658e2
Rahul Bansal <42.rahulbansal@gmail.com>
2023-10-20 01:35:37 +0530

    Import all pages and some components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import {
+	Ecommerce,
+	Orders,
+	Calendar,
+	Employees,
+	Stacked,
+	Customers,
+	Pyramid,
+	Kanban,
+	Area,
+	Bar,
+	Pie,
+	Financial,
+	ColorMapping,
+	Line,
+	ColorPicker,
+	Editor,
+} from "./pages";
 import "./App.css";
 
 const App = () => {
